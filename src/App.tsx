a98ac17ef60dc218bd5de70b072b48a9292d2357
longgoll <luuhoanglong21@gmail.com>
2025-02-25 14:08:28 +0700

    refactor: clean up App component and remove unused buildApi.json file

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,16 @@
-import { Routes, Route } from 'react-router-dom'
-import Home from '@/pages/Home/Home'
-import About from '@/pages/About/About'
-import NotFound from '@/pages/NotFound/NotFound'
+import { Routes, Route } from "react-router-dom";
+import Home from "@/pages/Home/Home";
+import About from "@/pages/About/About";
+import NotFound from "@/pages/NotFound/NotFound";
 
 function App() {
-
-  
-  
-
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
